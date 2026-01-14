1e02ea9797846678030f8f4a1aa3053ff78f6cd2
Psychedelic Jesus <quantumbraid@gmail.com>
2026-01-13 19:09:53 -0800

    tada

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -25,9 +25,9 @@
 // Graphic Readdressable Indexed Nodes - Android/Kotlin Implementation
 
 plugins {
-    id("com.android.library") version "8.2.0" apply false
-    id("com.android.application") version "8.2.0" apply false
-    kotlin("android") version "1.9.21" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("com.android.application") version "8.2.2" apply false
+    kotlin("android") version "1.9.22" apply false
     id("de.mannodermaus.android-junit5") version "1.10.0.0" apply false
     id("org.jetbrains.dokka") version "1.9.20" apply false
 }
